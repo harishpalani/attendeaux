421f0a4c1616552a8f20ad7a61bc4471763f9181
Richard <richardliuwa@gmail.com>
2018-09-25 05:16:57 -0700

    changed python file

--- example.py
+++ example.py
@@ -7,15 +7,25 @@ credentials = ServiceAccountCredentials.from_json_keyfile_name('attendeaux-d97bb
 
 gc = gspread.authorize(credentials)
 
-wks = gc.open('Test').sheet1
+wks = gc.open('Special Topics Attendance').sheet1
 
 # print(wks.get_all_records())
 # for i in range(2, 54):
 #     wks.update_cell(i, 4, '=FLOOR(SUM(F' + str(i) + ':S' + str(i) + '))')
 
+# for i in range(2, 54):
+#     wks.update_cell(i, 5, '=MOD(SUM(F' + str(i) + ':S' + str(i) + '),1)*10')
+
+attendance_file = open('test.txt', 'r')
+
+ls = []
+ls += [1] * 54
+
+while True:
+    line = attendance_file.readline()
+    if len(line) == 0:
+        break
+    ls[int(line)] = 0
+
 for i in range(2, 54):
-    wks.update_cell(i, 5, '=MOD(SUM(F' + str(i) + ':S' + str(i) + '),1)*10')
-
-# wks.delete_row(2)
-
-# wks.append_row(['sup'])
+    wks.update_cell(i, 6, ls[i])
